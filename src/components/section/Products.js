3c3019ac47fbfa0fd5121da2d756bddbc63d46f5
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-15 18:40:41 +0000

    Update

---
+++ src/components/section/Products.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {DataContext} from '../Context'
+import { Link } from 'react-router-dom';
+
+export class Products extends Component {
+
+    static contextType = DataContext;
+
+    render() {
+        const {products} = this.context;
+        return (
+           <div id="product">
+               {
+                 products.map(product =>(
+                     <div className="card" key={product._id}>
+                      <Link to={'/product/${product._id}'}>
+                          <img src={product.src} alt=""/>
+                      </Link>
+                     </div>
+                 ))
+               }               
+           </div>
+        )
+    }
+}
+
+export default Products;
