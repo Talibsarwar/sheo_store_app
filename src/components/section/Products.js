c2bb41bd8fcaa066c1ee1d44961dfc068883a9e5
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-16 05:24:40 +0000

    Update

--- src/components/section/Products.js
+++ src/components/section/Products.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
-import {DataContext} from '../Context'
+import { DataContext } from '../Context'
 import { Link } from 'react-router-dom';
+import '../css/Products.css'
 
 export class Products extends Component {
 
     static contextType = DataContext;
 
     render() {
-        const {products} = this.context;
+        const { products } = this.context;
         return (
-           <div id="product">
-               {
-                 products.map(product =>(
-                     <div className="card" key={product._id}>
-                      <Link to={'/product/${product._id}'}>
-                          <img src={product.src} alt=""/>
-                      </Link>
-                     </div>
-                 ))
-               }               
-           </div>
+            <div id="product">
+                {
+                    products.map(product => (
+                        <div className="card" key={product._id}>
+                            <Link to={'/product/${product._id}'}>
+                                <img src={product.src} alt="" />
+                            </Link>
+                            <div className="content">
+                                <h3>
+                                    <Link to={'/product/${product._id}'}>
+                                        {product.title}
+                                    </Link>
+                                </h3>
+                                <span>${product.price}</span>
+                                <p>{product.description}</p>
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         )
     }
 }
