bc657a915a60d7126330d184f8b481ded70d6b85
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-22 09:36:02 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import Products from './components/Products';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login_Registration from './components/Login or Registration';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DataProvider } from './components/DataProvider';
 import Details from './components/Details';
@@ -8,19 +11,22 @@ import './App.css';
 
 function App() {
   return (
-      <DataProvider>
-        <div className="App">
-          <Router>
-            <Header />
-            <section>
-              <Routes>
-                <Route path="products" element={<Products />} />
-                <Route path="products/:id" element={<Details />} />
-              </Routes>
-            </section>
-          </Router>
-        </div>
-      </DataProvider>
+    <DataProvider>
+      <div className="App">
+        <Router>
+          <Header />
+          <section>
+            <Routes>
+              <Route path="products" element={<Products />} />
+              <Route path="products/:id" element={<Details />} />
+              <Route path="products" element={<About />} />
+              <Route path="products" element={<Contact />} />
+              <Route path="products" element={<Login_Registration />} />
+            </Routes>
+          </section>
+        </Router>
+      </div>
+    </DataProvider>
   );
 }
 
---
+++ src/components/Login or Registration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login_Registration(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Login_Registration;
