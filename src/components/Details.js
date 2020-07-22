6aab199248ce30e94aeebcf82e2d1b20c1c8cbc5
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-22 06:44:14 +0000

    Update

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,17 +9,17 @@ export default function Details() {
     const { id } = useParams();
     const [products] = useContext(DataContext);
     const [index, setIndex] = useState(0)
-    const imgDiv = useRef();
+    const srcDiv = useRef();
 
     const details = products.filter((product, index) => {
         return product._id === id
     })
 
     const handleMouseMove = e => {
-        const { left, top, width, height } = e.target.getBounding.ClientRect();
+        const { left, top, width, height } = e.target.getBoundingClientRect();
         const x = (e.pageX - left) / width * 100
         const y = (e.pageY - top) / height * 100
-        imgDiv.current.style.backgroundPosition = `${x}% ${y}%`
+        srcDiv.current.style.backgroundPosition = `${x}% ${y}%`
     }
     return (
         <>
@@ -27,18 +27,18 @@ export default function Details() {
                 details.map(product => (
                     <div className="details" key={product._id}>
                         <div className="img-container" onMouseMove={handleMouseMove}
-                            style={{ backgroundImage: `url(${product.src[index]})` }} ref={imgDiv}
-                            onMouseLeave={() => imgDiv.current.style.backgroundPosition = `center`} />
+                            style={{ backgroundImage: `url(${product.src[index]})` }} ref={srcDiv}
+                            onMouseLeave={() => srcDiv.current.style.backgroundPosition = `center`} />
                         <div className="box-details">
                             <h2 title={product.title}>{product.title}</h2>
                             <h3>${product.price}</h3>
                             <Colors colors={product.colors} />
                             <Sizes sizes={product.sizes} />
                             <DetailsThumb src={product.src} setIndex={setIndex} />
+                            <p>{product.description}</p>
+                            <p>{product.content}</p>
+                            <button className="cart">Add to cart</button>
                         </div>
-                        <p>{product.description}</p>
-                        <p>{product.content}</p>
-                        <button className="cart">Add to cart</button>
                     </div>
                 ))
             }
