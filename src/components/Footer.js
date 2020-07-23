63428e552351879f164f670f4b78b0d7ab5c6f8f
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-23 11:17:16 +0000

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,8 +27,8 @@ function Footer() {
                 </div>
                 <div className="w-full pt-6 text-center md:text-right flex md:flex-row justify-center">
                     <ul className="text-white">
-                        <li><a href="https://www.linkedin.com/in/talib-sarwar-338417189/" target="_blank" rel="noopener noreferrer">Talibsarwar<img src={Linkedin} alt="LinkedIn" width="30"/></a></li>
-                        <li><a href="https://github.com/Talibsarwar" target="_blank" rel="noopener noreferrer">Talibsarwar<img src={Github} alt="Github" width="30"/></a></li>
+                        <li><Link className="hover:text-primary transition duration-500" to="https://www.linkedin.com/in/talib-sarwar-338417189/" target="_blank" rel="noopener noreferrer">Talibsarwar<img src={Linkedin} alt="LinkedIn" width="30"/></Link></li>
+                        <li><Link className="hover:text-primary  transition duration-500" to="https://github.com/Talibsarwar" target="_blank" rel="noopener noreferrer">Talibsarwar<img src={Github} alt="Github" width="30"/></Link></li>
                     </ul>
                 </div>
             </div>
