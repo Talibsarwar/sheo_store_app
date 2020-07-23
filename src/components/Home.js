6cc756084096789bddaf8b2d8bdf6b3043671be2
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-07-23 10:29:41 +0000

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <img src={require("../images/ShoeBanner.png")} alt="Shoes" width="895px" height="900px"  className="Banner"/>
+    <div className="Banner">
+      <img src={require("../images/ShoeBanner.png")} alt="Shoes"/>
     </div>
   );
 }
